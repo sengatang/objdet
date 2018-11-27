a8162ae8a1b495b83e6e00300a0c36768f87c10e
senga <sengattt@gmail.com>
2018-11-27 14:39:33 +0800

    add git ignore and encapsulate it into a function

--- image.py
+++ image.py
@@ -26,46 +26,48 @@ def bias_coin():
 #     return imglist
 
 
-global count_overall
-count_overall = 700
-
-for i in range(10,30):
-    print(i, datetime.datetime.now())
-    url = getURL('狗', i+1)
-    data = open_url(url)
-    pic_urls = re.findall('"objURL":"(.*?)",', data, re.S)
-    for pic in pic_urls:
-        if count_overall % 100 == 0:
+def crawl_pic(base_path, keyword, page_num):
+    global count_overall
+    count_overall = 0
+    for i in range(0,page_num):
+        print(i, datetime.datetime.now())
+        url = getURL(keyword, i+1)
+        data = open_url(url)
+        pic_urls = re.findall('"objURL":"(.*?)",', data, re.S)
+        
+        for pic in pic_urls:
+            if count_overall % 100 == 0:
+                try:
+                    os.makedirs(base_path + str(count_overall//100))
+                except Exception as e:
+                    print(e)
+                    pass
+                try:
+                    os.makedirs(base_path + str(count_overall//100) + '/training')
+                    os.makedirs(base_path + str(count_overall//100) + '/test')
+                except Exception as e:
+                    print(e)
+                    pass
+            root_dir = base_path + str(count_overall//100)
+            inside_dir = '/training' if not bias_coin() else '/test'
+            dir_path = root_dir + inside_dir
             try:
-                os.makedirs(str(count_overall//100))
+                pic = requests.get(pic, timeout=15)
+            except:
+                continue
+            fiel_name =str(count_overall + 1) + '.jpg'
+            dir_path = dir_path + '/' + fiel_name
+            try:
+                with open(dir_path, 'wb') as f:
+                    f.write(pic.content)
             except Exception as e:
                 print(e)
-                pass
-            try:
-                os.makedirs(str(count_overall//100) + '/training')
-                os.makedirs(str(count_overall//100) + '/test')
-            except Exception as e:
-                print(e)
-                pass
-        root_dir = str(count_overall//100)
-        inside_dir = '/training' if not bias_coin() else '/test'
-        dir_path = root_dir + inside_dir
-        try:
-            pic = requests.get(pic, timeout=15)
-        except:
-            continue
-        fiel_name =str(count_overall + 1) + '.jpg'
-        dir_path = dir_path + '/' + fiel_name
-        try:
-            with open(dir_path, 'wb') as f:
-                f.write(pic.content)
-        except Exception as e:
-            print(e)
-            continue
-        count_overall += 1
+                continue
+            count_overall += 1
 
 
-
+base_path = 'img file/bicycle/'
+crawl_pic(base_path, '自行车', 20)
 
 
     
